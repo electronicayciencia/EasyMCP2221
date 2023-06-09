628d1e9f955dd4f0643f6ad2e6c735c0a2f4fb2e
Electronica y Ciencia <electronicayciencia@gmail.com>
2023-06-10 00:42:30 +0200

    DS1307 still not working.

--- examples/clock/clock.py
+++ examples/clock/clock.py
@@ -1,10 +1,21 @@
-# Digital clock with a DS1307 and LCD dysplay
+# Digital clock with a DS1307 and LCD display with PCF8574 I2C adapter.
 from EasyMCP2221 import SMBus
 from lcd_driver import LCD
+from DS1307 import DS1307
+
 
 bus = SMBus(1)
 lcd = LCD(bus)
+ds = DS1307(bus)
+
 
 lcd.clear()
-lcd.display_string("hola", 1)
+#lcd.display_string("hola", 1)
+
+print(ds.read_datetime())
+
+if ds.halted():
+    ds.write_now()
+    
+print(ds.read_datetime())
 
