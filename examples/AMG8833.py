89f87469f28c4044e1bcca1ad71f105c2f0b4914
Electronica y Ciencia <electronicayciencia@gmail.com>
2023-06-26 17:40:37 +0200

    Negative numbers

--- examples/AMG8833.py
+++ examples/AMG8833.py
@@ -9,13 +9,16 @@ mcp = EasyMCP2221.Device()
 mcp.set_pin_function(
     gp0 = "GPIO_IN",
     gp1 = "GPIO_IN",
-    gp2 = "GPIO_IN",
+    gp2 = "GPIO_OUT", out2 = 0,
     gp3 = "LED_I2C")
 
 
-mcp.I2C_speed(400_000)
-sensor = mcp.I2C_Slave(0x69)
+mcp.GPIO_write(gp2=True)  # restart the device just in case
+sleep(0.1)
 
+mcp.I2C_speed(400_000)
+
+sensor = mcp.I2C_Slave(0x69)
 
 img = None
 while True:
@@ -23,7 +26,7 @@ while True:
     thr_tmp = int.from_bytes(data, byteorder="little", signed=True) * 0.0625
 
     data = sensor.read_register(0x80, 128)
-    T = 0.25 * np.frombuffer(data, dtype=np.int16).reshape(8,8)
+    T = 0.25/16 * (np.frombuffer(data, dtype=np.int16).reshape(8,8) << 4)
     T = np.flipud(T)
     T = np.fliplr(T)
 
@@ -35,6 +38,6 @@ while True:
 
     else:
         img.set_data(T)
-        cbar.mappable.set_clim(vmin=27, vmax=33)
+        cbar.mappable.set_clim(vmin=20, vmax=34)
 
     plt.pause(0.01)
