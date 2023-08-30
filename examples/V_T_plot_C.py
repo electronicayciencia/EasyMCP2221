21ee6701722cbc212239500cbb4c9a44f93b621c
Electronica y Ciencia <electronicayciencia@gmail.com>
2023-08-30 22:12:00 +0200

    Simple C charging plot

--- examples/V_T_plot_C.py
+++ examples/V_T_plot_C.py
@@ -1,24 +1,31 @@
 # Plotter for capacitor change/discharge
 import EasyMCP2221
 import time
+import math
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 import numpy as np
 
 capture_time = 1
-Vdd = 5
 
 # Configure device pins
 mcp = EasyMCP2221.Device()
+mcp.set_pin_function(
+    gp2 = "GPIO_OUT",
+    gp3 = "ADC")
+
 mcp.ADC_config()
-mcp.set_pin_function(gp2 = "GPIO_OUT", gp3 = "ADC")
+
 
 V = []
 T = []
 
-print("Initial discharge on course. Press enter to start charging.")
+print("Discharging...")
 mcp.GPIO_write(gp2 = False)
 
-input()
+while mcp.ADC_read()[2] / 1024:
+    pass
+
 print("Charging...")
 mcp.GPIO_write(gp2 = True)
 
@@ -27,24 +34,29 @@ start = time.perf_counter()
 while time.perf_counter() - start <= capture_time:
 
     t = time.perf_counter()
-    (_, _, V3) = mcp.ADC_read()
+    V = mcp.ADC_read()[2]
 
-    # 10 bit, 5V ref
-    V3 = V3 / 1024 * Vdd
+    # 10 bit
+    V = V / 1024 * 100
 
     T.append(t - start)
-    V.append(V3)
+    V.append(V)
 
 
 mcp.GPIO_write(gp2 = False)
 
-
 plt.plot(T, V, 'o-')
-plt.axis([-0.05, capture_time, 0, Vdd + 0.5])
-plt.xticks(np.arange(0,capture_time,0.1))
+plt.axis([-0.05, capture_time, 0, 105])
+plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter())
+plt.gca().xaxis.set_major_locator(mtick.MultipleLocator(0.1))
+
+#plt.xticks(np.arange(0,capture_time,0.1))
 plt.xlabel("Time (s)")
-plt.ylabel("V (V)")
+plt.ylabel("V (%)")
 plt.title("Capacitor charge plot")
+
+# 1st RC line
+plt.axhline(y = 100 * (1-1/math.e) , color='g', linestyle='dotted')
+
 plt.grid()
 plt.show()
-
