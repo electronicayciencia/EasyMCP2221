30f1a04f6d67f7199e0aff5317781badbfac3fd1
Electronica y Ciencia <electronicayciencia@gmail.com>
2023-10-29 12:53:03 +0100

    LCD testing

---
+++ examples/lcd/testing.py
@@ -0,0 +1,31 @@
+from EasyMCP2221 import SMBus
+from lcd_driver import LCD
+
+bus = SMBus()
+lcd = LCD(bus, addr=0x27)
+
+bus.mcp.set_pin_function(
+    gp0 = "GPIO_IN",  # unused
+    gp1 = "GPIO_IN",  # unused
+    gp2 = "GPIO_IN",  # unused
+    gp3 = "LED_I2C")  # i2c traffic indicator
+
+
+lcd.clear()
+
+lcd.display_string("Electronica", 1)
+lcd.display_string(" y ciencia   EyC", 2)
+
+lcd.clear()
+
+#                   1234567890123456
+lcd.display_string(" Blog EyC  2023 ", 1)
+lcd.display_string("                ", 2)
+
+exit()
+
+i = 1
+while True:
+    #lcd.display_string("Counter... %4d" % (i), 1)
+    #lcd.display_string("Counter... %4d" % (i), 1)
+    i = i + 1
